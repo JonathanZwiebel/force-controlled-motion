8c20e328cd9c74b7af435ec6a0dcdd3cc3f5b35e
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2015-11-12 16:51:11 -0800

    Created Force, ForceDirectedBody and BidirectionalForce classes

---
+++ src/BidirectionalForce.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Jonathan Zwiebel on 11/12/2015.
+ */
+public class BidirectionalForce extends Force {
+}
---
+++ src/ForceDirectedBody.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Jonathan Zwiebel on 11/12/2015.
+ */
+public abstract class ForceDirectedBody {
+}
