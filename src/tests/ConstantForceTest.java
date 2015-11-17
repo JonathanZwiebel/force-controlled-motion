5bdab91ce127f264046353a11779a9d7690aef28
jdrumelhart <jdrumelhart@yahoo.com>
2015-11-17 15:05:46 -0800

    moved files from default package to main

--- src/tests/ConstantForceTest.java
+++ src/tests/ConstantForceTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 
 public class ConstantForceTest extends OneSpaceForceTest {
 
+	
 	@Test
 	public void testStep() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testConstantForce() {
-		fail("Not yet implemented");
+		
 	}
 
 }
